509c59b5dbe4a42194273c598663cbaa0b1882fc
danieladeboladada <daniel.dada129@gmail.com>
2025-11-13 21:34:38 -0500

    set up mongo db cluster and connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+console.log(process.env.MONGO_URI);
+
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    connectDB();
+    console.log('Server is running on port 3000');
 });
